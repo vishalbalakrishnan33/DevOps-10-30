e513f3870b1883b91584ac82723e842212868397
Bhuvana <vishalbalakrishnan33@gmail.com>
2019-12-29 21:59:22 +0530

    added all files

---
+++ myFirst/src/Firstjavaprg/myFirstjava.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package Firstjavaprg;
+
+/**
+ * @author VIRUS PC
+ *
+ */
+public class myFirstjava {
+
+	/**
+	 * 
+	 */
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(		"Bhuvana'");
+	}
+
+}
---
+++ myTest/src/myTest/myTestone.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package myTest;
+  import java.util.Date;
+
+/**
+ * @author VIRUS PC
+ *
+ */
+public class myTestone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(getLocalCurrentDate());
+
+	}
+	private static Date getLocalCurrentDate() {
+		return new Date();		
+	}
+
+}
